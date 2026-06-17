a193bd37d03dbf70391e7d18fa0f487b48c39326
Vedant Maheshwari <vedantmaheshwari2006@gmail.com>
2026-06-17 13:47:55 +0530

    button added

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 
-function changeBackgroundImage(){
+function changeBackgroundImage() {
     let currentDateTime = new Date();
     let hrs = currentDateTime.getHours();
     let completeBody = document.getElementsByTagName("body")[0];
-    let clock = document.getElementById("digital-clock");   
-    
-    if(hrs>=18){
+    let clock = document.getElementById("digital-clock");
+
+    if (hrs >= 18) {
         //night image
         completeBody.style.backgroundImage = 'url("https://res.cloudinary.com/dh62nngtk/image/upload/v1781606137/billy-huynh-W8KTS-mhFUE-unsplash_hlxjf5.jpg")';
         clock.classList.add("night");
 
     }
-    else{
+    else {
         //day image
         completeBody.style.backgroundImage = 'url("https://res.cloudinary.com/dh62nngtk/image/upload/v1781605693/jonas-weckschmied--N_UwPdUs7E-unsplash_mf2yze.jpg")';
         clock.classList.remove("night");
@@ -19,15 +19,30 @@ function changeBackgroundImage(){
     }
 }
 
-function uploadTime(){
+function uploadTime() {
     let currentDateTime = new Date();
     let hr = document.getElementById("hrs");
     let min = document.getElementById("mins");
     let seconds = document.getElementById("seconds");
 
-    hr.textContent=currentDateTime.getHours();
-    min.textContent=currentDateTime.getMinutes();
-    seconds.textContent=currentDateTime.getSeconds();
+    hr.textContent = currentDateTime.getHours();
+    min.textContent = currentDateTime.getMinutes();
+    seconds.textContent = currentDateTime.getSeconds();
 }
 changeBackgroundImage();
-setInterval(uploadTime,1000);
+setInterval(uploadTime, 1000);
+
+let analog_btn = document.getElementById("analog");
+analog_btn.addEventListener("click", () => {
+    let digital_clock = document.getElementById("digital-clock");
+    let analog_clock = document.getElementById("analog-clock");
+    digital_clock.style = "display:none";
+    analog_clock.style = "display:flex"
+})
+let digital_btn = document.getElementById("digital");
+digital_btn.addEventListener("click",()=>{
+    let digital_clock = document.getElementById("digital-clock");
+    let analog_clock = document.getElementById("analog-clock");
+    digital_clock.style = "display:flex";
+    analog_clock.style = "display:none"
+})
